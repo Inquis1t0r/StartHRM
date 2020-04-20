046f5d40d8f6f07dec43cbe855cba3a8da6356d1
Bartłomiej Komendarczuk <bkomendarczuk@gmail.com>
2020-04-20 16:22:07 +0200

    Details about contract terms

--- modules/server/src/pl/belluu/server/employee/EmployeeContact.java
+++ modules/server/src/pl/belluu/server/employee/EmployeeContact.java
@@ -1,10 +1,19 @@
 package pl.belluu.server.employee;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.context.annotation.ComponentScan;
+
+import lombok.Data;
 
 @Entity
 @Data
-@Table(name= "EmpCont")
+@Table(name= "EmpContact")
 @ComponentScan
 public class EmployeeContact {
 
---
+++ modules/server/src/pl/belluu/server/employee/EmployeeContractTerms.java
@@ -0,0 +1,45 @@
+package pl.belluu.server.EmployeeContractTerms;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.context.annotation.ComponentScan;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name= "EmpContractTerms")
+@ComponentScan
+public class EmployeeContractTerms {
+
+    @Id
+    @Column(name = "EMT_Id")
+    private int id;
+
+    @Column(name = "EMT_StartDate")
+    private Date startDate;
+
+    @Column(name = "EMT_EndDate")
+    private Date endDate;
+
+    @Column(name = "EMT_PositionHeld")
+    private String positionHeld;
+
+    @Column(name = "EMT_NetSalary")
+    private String netSalary;
+
+    @Column(name = "EMT_GrossSalary")
+    private String grossSalary;
+
+    @OneToOne
+    @MapsId
+    private Employee employee;
+
+}
